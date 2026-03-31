c502c4c82dc124224bedc2e287e365a1008d1c40
mado <seeyoutnk@gmail.com>
2026-04-01 00:59:37 +0900

    .

--- src/data/presets/Eizo/26EizoFall.ts
+++ src/data/presets/Eizo/26EizoFall.ts
@@ -4,9 +4,5 @@ import { FacultyTimetablePresets } from '../../../types';
  * 26年度 映像学部 秋セメスター プリセット
  */
 export const presets: FacultyTimetablePresets = {
-  '0': { // 月曜日
-    '0': [ // 1限
-      { code: '60101', name: '映像制作概論(秋)', professor: '大学六郎', location: 'F棟 401教室', syllabusUrl: 'https://syllabus.ritsumei.ac.jp/' }
-    ]
-  }
+  
 };
--- src/data/presets/Seisaku/26SeisakuFall.ts
+++ src/data/presets/Seisaku/26SeisakuFall.ts
@@ -4,9 +4,5 @@ import { FacultyTimetablePresets } from '../../../types';
  * 26年度 政策科学部 秋セメスター プリセット
  */
 export const presets: FacultyTimetablePresets = {
-  '0': { // 月曜日
-    '0': [ // 1限
-      { code: '20101', name: '政策科学概論(秋)', professor: '大学六郎', location: 'F棟 201教室', syllabusUrl: 'https://syllabus.ritsumei.ac.jp/' }
-    ]
-  }
+  
 };
