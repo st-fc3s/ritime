b6250bf8a3bc4686303cb42b2d37225e9e01723e
mado <seeyoutnk@gmail.com>
2026-04-01 02:22:34 +0900

    .

--- src/data/presets/Keiei/26KeieiFall.ts
+++ src/data/presets/Keiei/26KeieiFall.ts
@@ -4,4 +4,9 @@ import { FacultyTimetablePresets } from '../../../types';
  * 26年度 経営学部 秋セメスター プリセット
  */
 export const presets: FacultyTimetablePresets = {
+    '0': { // 月曜日
+        '0': [ // 1限
+            { code: '10101', name: '経営学概論(秋)', professor: '大学二郎', location: 'B棟 101教室', syllabusUrl: 'https://syllabus.ritsumei.ac.jp/' }
+        ]
+    }
 };
